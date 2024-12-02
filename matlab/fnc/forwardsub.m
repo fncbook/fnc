7cd9b2afca3bdfe1efb44fbe54671b2208a9f281
Toby Driscoll <driscoll@udel.edu>
2024-12-02 11:07:25 -0500

    added matlab to chapter 2

--- matlab/fnc/forwardsub.m
+++ matlab/fnc/forwardsub.m
@@ -9,5 +9,5 @@ function x = forwardsub(L,b)
 n = length(L);
 x = zeros(n,1);
 for i = 1:n
-  x(i) = ( b(i) - L(i,1:i-1)*x(1:i-1) ) / L(i,i);
+  x(i) = ( b(i) - L(i,1:i-1) * x(1:i-1) ) / L(i,i);
 end
